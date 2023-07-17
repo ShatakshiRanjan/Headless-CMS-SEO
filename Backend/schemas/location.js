fdab0fb99a6a93f2474662b88f8d16ca9adc1508
Shatakshi Ranjan <106120628+ShatakshiRanjan@users.noreply.github.com>
2023-07-17 14:52:54 -0400

    minor update

--- Backend/schemas/location.js
+++ Backend/schemas/location.js
@@ -5,6 +5,19 @@ export default {
 	title: 'Location',
   fields: [
     {
+      name: 'slug',
+      title: 'URL Slug',
+      type: 'slug',
+      options: {
+        source: function (loc) {
+          // Combine the city and state fields to generate the slug
+          return `${loc.city} ${loc.state}`;
+        },
+        maxLength: 200, // Specify the maximum length of the slug if desired
+        slugify: input => input.toLowerCase().replace(/\s+/g, '-').slice(0, 200) // Customize slug generation as needed
+      }
+    },
+    {
       name: 'city',
       type: 'string',
       title: 'City'
@@ -13,19 +26,6 @@ export default {
       name: 'state',
       type: 'string',
       title: 'State',
-    },
-    {
-      name: 'slug',
-      title: 'URL Slug',
-      type: 'slug',
-      options: {
-        source: function (doc) {
-          // Combine the city and state fields to generate the slug
-          return `${doc.city} ${doc.state}`;
-        },
-        maxLength: 200, // Specify the maximum length of the slug if desired
-        slugify: input => input.toLowerCase().replace(/\s+/g, '-').slice(0, 200) // Customize slug generation as needed
-      }
     },
   ]
 }
